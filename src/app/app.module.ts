10de4ea1d2ef84d7544e8666dfc5d392022f07c2
Caine Cridland <cainecridland@gmail.com>
2021-09-06 18:39:05 +0100

    Add sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { IntroComponent } from './profile/intro/intro.component';
+import { ExperienceComponent } from './profile/experience/experience.component';
+import { SkillsComponent } from './profile/skills/skills.component';
+import { ToolsComponent } from './profile/tools/tools.component';
+import { ProjectsComponent } from './profile/projects/projects.component';
+import { ContactModalComponent } from './contact-modal/contact-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ProfileComponent,
+    IntroComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ToolsComponent,
+    ProjectsComponent,
+    ContactModalComponent
   ],
   imports: [
     BrowserModule
